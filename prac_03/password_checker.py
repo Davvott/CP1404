237cd9b0f36f98b3f282bc918a4bdc2b04044cee
Sassafrass <stephen.davey@my.jcu.edu.au>
2019-03-15 10:01:45 +1000

    Refactor functions from code

---
+++ prac_03/password_checker.py
@@ -0,0 +1,23 @@
+MIN_LEN = 4
+
+def main():
+    pw = get_password()
+
+    print_password(pw)
+
+
+def print_password(pw):
+    print("*"*len(pw))
+    # for i in range(len(pw)):
+    #     print("*")
+
+
+def get_password():
+    pw = input("Enter your password: ")
+    while len(pw) < MIN_LEN:
+        print("Your password must be at least {}".format(MIN_LEN))
+        pw = input("Enter your password: ")
+    return pw
+
+
+main()
