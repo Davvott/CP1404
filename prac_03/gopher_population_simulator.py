d1fd60828c9ed8bebcbd4a52bc20fd0f14fe1898
Davvott <stephen.davey@my.jcu.edu.au>
2019-03-21 12:45:15 +1000

    Refactor naming consistency

--- prac_03/gopher_population_simulator.py
+++ prac_03/gopher_population_simulator.py
@@ -1,4 +1,6 @@
 """
+CP1404 - Prac_03 - IT@JCU
+
 A secret population of 1000 gophers lives near the library.
 Every year, a random number of gophers is born, between 10% of the current
 population, and 20%. (e.g. 15% of the gophers might give birth,
@@ -28,8 +30,8 @@ Population: 847
 
 import random
 
-PERIOD = 10
-BIRTH_RANGE = [.1, .21]
+PERIOD_IN_YEARS = 10
+BIRTHS_RANGE = [.1, .21]
 DEATHS_RANGE = [.05, .25]
 
 
@@ -38,20 +40,20 @@ def main():
     print("Welcome to the Gopher Population Simulator!")
     print("Starting population: {}".format(population))
 
-    for y in range(1, PERIOD):
+    for year in range(1, PERIOD_IN_YEARS):
         births, deaths = calc_pop_change(population)
         population += (births - deaths)
 
         print("{} gophers were born. {} died.".format(births, deaths))
         print("Population: {}".format(population))
-        print("Year {}\n".format(y))
+        print("Year {}\n".format(year))
 
 
 def calc_pop_change(population):
     """Calculate population change, return births and deaths"""
-    born = round(random.uniform(BIRTH_RANGE[0], BIRTH_RANGE[1]) * population)
+    births = round(random.uniform(BIRTHS_RANGE[0], BIRTHS_RANGE[1]) * population)
     deaths = round(random.uniform(DEATHS_RANGE[0], DEATHS_RANGE[1]) * population)
-    return born, deaths
+    return births, deaths
 
 
 main()
