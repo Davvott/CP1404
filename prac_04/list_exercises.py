a083bddb9bd173237b79314571ba36f80de3fbc5
Sassafrass <stephen.davey@my.jcu.edu.au>
2019-03-22 11:04:30 +1000

    Extend list_exercise with indefinite loop + try/except

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,15 +1,28 @@
 # Prompt user for 5 integers and store in list
 numbers = []
 
-for num in range(5):
-    user_input = int(input("Number: "))
-    numbers.append(user_input)
+continue_prompting = True
+count = 1
+while continue_prompting:
+    try:
+        user_input = int(input("Number {}: ".format(count)))
+        if user_input < 0:
+            continue_prompting = False
+        else:
+            numbers.append(user_input)
+            count += 1
+    except ValueError:
+        print("Invalid input, try again")
+
+# for num in range(5):
+#     user_input = int(input("Number: "))
+#     numbers.append(user_input)
 
 print("The first number is {}".format(numbers[0]))
 print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
-print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+print("The average of the numbers is {:.2f}".format(sum(numbers)/len(numbers)))
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
              'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command',
