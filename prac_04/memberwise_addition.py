a72c2564ae871912364d92795b29e81018d3938d
jc345887 <stephen.davey@my.jcu.edu.au>
2019-03-28 12:29:51 +1000

    Update add_memberwise.py with nested zip in list comp

---
+++ prac_04/memberwise_addition.py
@@ -0,0 +1,12 @@
+
+def add_memberwise(list_1, list_2):
+    # Add each indice of list together
+    added = list((zip(list_1, list_2)))
+    summed = [sum(list(i)) for i in added]
+    comp = [sum(list(element)) for element in zip(list_1, list_2)]
+    return comp
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+
+print(add_memberwise(l1, l2))
