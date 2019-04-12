e9d30991d0b5b3b9936ddad9d519fb896f73b48a
Sassafrass <stephen.davey@my.jcu.edu.au>
2019-04-12 11:00:28 +1000

    Add docstrings and update int_check to use class variable

--- prac_07/convert_miles_kms.py
+++ prac_07/convert_miles_kms.py
@@ -5,6 +5,7 @@ from kivy.app import StringProperty
 CONVERSION_NUM = 1.60934
 class ConvertMilesKms(App):
 
+
     output_text = StringProperty()
 
     def build(self):
@@ -13,25 +14,29 @@ class ConvertMilesKms(App):
         return self.root
 
     def handle_increment(self, n):
+        """Validates text then increments and calls handle_convert_units()"""
         if self.handle_int_check():
             unit = int(self.root.ids.text_input.text)
             unit += n
+            # Update text input box
             self.root.ids.text_input.text = "{}".format(unit)
+            # Auto convert unit
             self.handle_convert_units()
 
     def handle_convert_units(self):
+        """Validates text, converts by constant, updates output_text"""
         if self.handle_int_check():
             unit = int(self.root.ids.text_input.text)
             unit *= CONVERSION_NUM
             self.output_text = str(unit)
 
     def handle_int_check(self):
-        # Try for int
+        """Checks text_input.text is int and returns True, else False"""
         try:
             int(self.root.ids.text_input.text)
             return True
         except ValueError as err:
-            self.root.ids.display_text.text = "0.0"
+            self.output_text = "0.0"
             pass
         return False
 
