200c63e8b3a4870ba329deba66534f5bab67317c
Sassafrass <stephen.davey@my.jcu.edu.au>
2019-05-08 16:33:00 +1000

    Refactor: Taxi get_fare return rounded fare; test code assertion

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -6,7 +6,7 @@ limo = SilverServiceTaxi("Stretch", 100, 2)
 
 limo.start_fare()
 limo.drive(18)
-assert limo.get_fare() == 48.80
+assert limo.get_fare() == 48.80  # Updated for Taxi.get_fare round() value
 print(limo)
 
 hummer = SilverServiceTaxi("Boss", 100, 4)
