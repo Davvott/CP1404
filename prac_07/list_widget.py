71784162010dfc4e43a54656a6d3e5673b49af02
jc345887 <stephen.davey@my.jcu.edu.au>
2019-05-09 12:39:47 +1000

    Fix bug in create_widget function; add docstrings

--- prac_07/list_widget.py
+++ prac_07/list_widget.py
@@ -1,3 +1,4 @@
+"""List Widget creator GUI. Create labels from text"""
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -11,26 +12,29 @@ canvas = "canvas:" \
          "      pos: self.pos" \
          "      size: self.size"
 
+
 class DynamicListApp(App):
     """Main program - Kivy app to demo dynamic widget creation."""
 
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
-        # basic data example - list of strings
-        self.strings = ["this", "is", "my", "list"]
-        print(self.strings)
+        self.strings = []
 
     def build(self):
         """Build the Kivy GUI."""
         self.title = "List Widget"
         self.root = Builder.load_file('list_widget.kv')
-
+        # Add initial widgets from default text in kv file
         self.create_widgets()
         return self.root
 
     def create_widgets(self):
+        self.strings = []
+        self.strings.append(self.root.ids.text_input.text)
+
         for string in self.strings:
+            # Create a label and add_widget to id:list_box
             temp_label = Label(text=string)
             # temp_label.bind(on_release=self.press_entry)
             self.root.ids.list_box.add_widget(temp_label)
